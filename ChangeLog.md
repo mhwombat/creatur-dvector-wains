884850583dbb3f4a69201e4f2347ca835fb476b1
Amy de Buitléir <amy@nualeargais.ie>
2018-11-01 10:18:28 +0000

    upgraded to lts-12.16

---
+++ ChangeLog.md
@@ -0,0 +1,6 @@
+# Changelog for creatur-dvector-wains
+
+1.1.0 Upgraded to lts-12.16.
+      Revamped to work with Stack v1.7.1.
+
+## Unreleased changes
---
+++ test/Spec.hs
@@ -0,0 +1,29 @@
+------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) Amy de Buitléir 2017-2018
+-- License     :  BSD-style
+-- Maintainer  :  amy@nualeargais.ie
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Runs the QuickCheck tests.
+--
+------------------------------------------------------------------------
+module Main where
+
+import ALife.Creatur.Wain.DVector.DoubleQC (test)
+import ALife.Creatur.Wain.DVector.PatternQC (test)
+
+import Test.Framework as TF (defaultMain, Test)
+
+tests :: [TF.Test]
+tests = 
+  [
+    -- In increasing order of complexity
+    ALife.Creatur.Wain.DVector.DoubleQC.test,
+    ALife.Creatur.Wain.DVector.PatternQC.test
+  ]
+
+main :: IO ()
+main = defaultMain tests
