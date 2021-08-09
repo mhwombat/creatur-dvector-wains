c99e32561eea7c2cbd54b89f3867562869ea3c3b
Amy de Buitléir <amy@nualeargais.ie>
2021-08-09 14:26:07 +0100

    refresh

--- src/ALife/Creatur/Wain/DVector/Wain.hs
+++ src/ALife/Creatur/Wain/DVector/Wain.hs
@@ -19,42 +19,20 @@ module ALife.Creatur.Wain.DVector.Wain
     packageVersion
   ) where
 
-import           ALife.Creatur                      (agentId)
 import qualified ALife.Creatur.Wain                 as W
 import           ALife.Creatur.Wain.Brain           (classifier)
 import           ALife.Creatur.Wain.DVector.Pattern (Pattern)
 import           ALife.Creatur.Wain.DVector.Tweaker (PatternTweaker (..))
 import           ALife.Creatur.Wain.GeneticSOM      (numModels)
-import           ALife.Creatur.Wain.UnitInterval    (uiToDouble)
 import           Control.Lens                       hiding (universe)
-import           Control.Monad.State.Lazy           (StateT)
 import           Data.Version                       (showVersion)
 import           Paths_creatur_dvector_wains        (version)
-import           Text.Printf                        (printf)
 
 -- | Returns the current version number of this library.
 packageVersion :: String
 packageVersion = "creatur-dvector-wains-" ++ showVersion version
 
 type PatternWain a rt m = W.Wain Pattern PatternTweaker rt m a
-
--- adjustEnergy
---   :: Simple Lens e (PatternWain a rt m) -> Double
---     -> Simple Lens s Double -> String -> Simple Lens e s
---       -> (String -> StateT e IO ()) -> StateT e IO ()
--- adjustEnergy
---     wainLens deltaE statLens reason summary report = do
---   w <- use wainLens
---   let (w', used) = W.adjustEnergy deltaE w
---   report $ "Adjusting energy of " ++ agentId w
---     ++ " because " ++ reason ++ ": "
---     ++ printf "%.3f" (uiToDouble . W._energy $ w)
---     ++ " + " ++ printf "%.3f" deltaE
---     ++ " -> " ++ printf "%.3f" (uiToDouble . W._energy $ w')
---     ++ " used=" ++ printf "%.3f" used ++ " leftover="
---     ++ printf "%.3f" (deltaE - used)
---   (summary . statLens) += used
---   assign wainLens w'
 
 metabCost :: Double -> Double -> Double -> PatternWain a rt m -> Double
 metabCost bmc cpcm scale w = scale * (bmc + cpcm * fromIntegral n)
